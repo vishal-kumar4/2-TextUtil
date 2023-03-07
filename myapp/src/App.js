517144525f8b983eba801804113e63b7464992dd
vishal-kumar4 <vishal8143667512@gmail.com>
2023-03-07 09:43:37 +0530

    created react app textUtil

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,10 +1,56 @@
 import './App.css';
+import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
+import About from './components/About';
+import React, { useState } from 'react';
+import Alert from './components/Alert';
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
 
+ 
 function App() {
+  const [mode, setMode] = useState('light'); // Whether dark mode is enabled or not
+  const [alert, setAlert] = useState(null);
+
+  const showAlert = (message, type)=>{
+      setAlert({
+        msg: message,
+        type: type
+      })
+      setTimeout(() => {
+          setAlert(null);
+      }, 1500);
+  }
+
+  const toggleMode = ()=>{
+    if(mode === 'light'){
+      setMode('dark');
+      document.body.style.backgroundColor = '#042743';
+      showAlert("Dark mode has been enabled", "success");
+    }
+    else{
+      setMode('light');
+      document.body.style.backgroundColor = 'white';
+      showAlert("Light mode has been enabled", "success");
+    }
+  }
   return (
-    <div className="App">
-      <h1 className='textcenter mt-3'>Hola fs</h1>
-    </div>
+    <>
+        <BrowserRouter>
+        <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} key={new Date()} />
+    <Alert alert={alert}/>
+      <div className="container my-3">
+      <Routes>
+        <Route path="/" element={<TextForm showAlert={showAlert} heading="Try TextUtils - word counter, character counter, remove extra spaces" mode={mode}/>}>
+        <Route path="/about" element={<About mode={mode} />} />
+        </Route>
+      </Routes>
+      </div>
+    </BrowserRouter>
+    </> 
   );
 }
 
